28279e31543f61e26ba715e172ab1e8ad470c502
Rixda9 <78431473+Rixda9@users.noreply.github.com>
2025-11-15 09:10:39 +0100

    Add command execution and cd built-in

--- shell.c
+++ shell.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void) {
 
     char input[1024];
 
-
     while (1) {
-
 
         const char *SEPARATOR = " ";
 
@@ -19,30 +21,60 @@ int main(void) {
         if(fgets(input, sizeof(input), stdin) == NULL) {
             break;
         }
-        // newline
+
+        // get rid of newline
         input[strcspn(input, "\n")] = 0;
+
 
         if (strcmp(input, "exit") == 0) {
             printf("Goodbye\n");
             break;
         }
+        // setup to use commands
         char *args[128];
         int argc = 0;
         char *strToken = strtok(input, SEPARATOR);
 
-        while (strToken != NULL)
-        {
+        // parse inputs
+        while (strToken != NULL) {
             args[argc] = strToken;
             argc++;
-            //printf("%s successfully copied\n", args[argc]);
             strToken = strtok( NULL, SEPARATOR);
         }
 
         args[argc] = NULL;
 
-        printf("Command: %s\n", args[0]);
-        for (int i = 0; args[i] != NULL; i++) {
-            printf(" args[%d] = %s\n", i, args[i]);
+        if (argc == 0){
+            continue;
+        }
+
+        // handle cd before forking
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL) {
+                fprintf(stderr, "cd: missing arguments\n");
+            }
+            else {
+                if (chdir(args[1])!= 0) {
+                    perror("cd");
+                }
+            }
+            continue;
+        }
+
+        // create a fork to execute the command in a different process
+        pid_t pid = fork();
+
+        if (pid<0) {
+            perror("fork failed");
+            exit(1);
+        }
+
+        else if (pid == 0){
+            execvp(args[0], args);
+            fprintf(stderr, "%s: command not found\n", args[0]);
+        }
+        else {
+            wait(NULL);
         }
     }
 }
